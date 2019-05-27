abae78db2f10d00e00f59f0b65ee4c3532887191
azzaall <azza.allouch@gmail.com>
2019-05-27 14:01:31 +0100

    Removed ardupilot Directory

--- ardupilot/AntennaTracker/radio.cpp
+++
@@ -1,8 +0,0 @@
-#include "Tracker.h"
-
-// Functions to read the RC radio input
-
-void Tracker::read_radio()
-{
-    RC_Channels::read_input();
-}
--- ardupilot/build/c4che/_cache.py
+++
@@ -1,7 +0,0 @@
-AUTOCONFIG = True
-BINDIR = '/usr/bin'
-BOARD = 'sitl'
-DEBUG = False
-LIBDIR = '/usr/lib'
-PREFIX = '/usr'
-VARIANT = 'sitl'
